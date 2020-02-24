3ef4cd8a2510a75f2b60474f282cac45839b11e5
Jaylon Slebo <jaylon.slebo.2023@student.lhschools.org>
2020-02-24 12:15:55 -0500

    Intelij Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,217 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Phase One//
+
+        String name;
+        String sport;
+        String food;
+        String gas;
+        String car;
+        String shoe;
+        String store;
+        String game;
+        String country;
+        String perfume;
+        String cologne;
+        String person;
+        String drink;
+        String fruit;
+        String vegetable;
+        String meat;
+        String animal;
+        String fish;
+        String bird;
+        String element;
+        String greeting;
+
+
+        //Phase 2
+        String Greeting;
+        String Name;
+        String userName;
+        Scanner Keyboard;
+        String color;
+        String favoriteSport;
+        String bestShoebrand;
+        String favoriteFood;
+        String favoriteSubject;
+        String favoriteMovie;
+        String favoriteTvshow;
+        String favoriteCar;
+        String favoriteComedian;
+        String favoriteRapper;
+        String Answer;
+        String favoriteActor;
+        String favoriteVideogame;
+        String favoriteRestaurant;
+        String Age;
+        String favoriteGamecharacter;
+        String favoriteMovieseries;
+
+        Greeting = "Hello";
+        Name = "My name is Jaylon";
+        Keyboard = new Scanner(System.in);
+
+        System.out.println(Greeting);
+        System.out.println("What's your name?");
+        Name = Keyboard.nextLine();
+        System.out.println("That a nice name");
+
+        System.out.println("What about your favorite color?");
+        color = Keyboard.nextLine();
+        System.out.println("Thats an alright color not as good as my favorite though.");
+
+        System.out.println("ok another question, what is your favorite sport?");
+        favoriteSport = Keyboard.nextLine();
+        System.out.println("What a coincidence same here!");
+
+        System.out.println("What is The Best shoe brand?");
+        bestShoebrand = Keyboard.nextLine();
+        System.out.println("Yeah, ok that's pretty good.");
+
+        System.out.println("What is your favorite food?");
+        favoriteFood = Keyboard.nextLine();
+        System.out.println("Oh thats really good!");
+
+        System.out.println("What is your favorite subject in school");
+        favoriteSubject = Keyboard.nextLine();
+        System.out.println("Ah, that class is hard.");
+
+        System.out.println("What is your favorite movie?");
+        favoriteMovie = Keyboard.nextLine();
+        System.out.println("Me and my family went out to see that!");
+
+        System.out.println("What is your favorite TV show?");
+        favoriteTvshow = Keyboard.nextLine();
+        System.out.println("That show is good, I hope they put out more seasons!");
+
+        System.out.println("What is the coolest car");
+        favoriteCar = Keyboard.nextLine();
+        System.out.println("My uncle used to own one of those it was really cool.");
+
+        System.out.println("Who is your favorite comedian?");
+        favoriteComedian = Keyboard.nextLine();
+        System.out.println("Oh I love them they are so funny");
+
+        System.out.println("Who is your favorite Rapper");
+        favoriteRapper = Keyboard.nextLine();
+        System.out.println("I loved his new Album have you heard it?");
+        Answer = Keyboard.nextLine();
+
+        System.out.println("Who is your favorite actor?");
+        favoriteActor = Keyboard.nextLine();
+        System.out.println("Oh I heard they are gonna be in the next star wars!");
+
+        System.out.println("What is your favorite video game?");
+        favoriteVideogame = Keyboard.nextLine();
+        System.out.println("alright thats understandable");
+
+        System.out.println("What's your favorite restaurant");
+        favoriteRestaurant = Keyboard.nextLine();
+        System.out.println("Oh i like their food!");
+
+        System.out.println("How old are you");
+        Age = Keyboard.nextLine();
+        System.out.println("Man you are pretty old for how you look!");
+
+        System.out.println("Who is your favorite character from a video game?");
+        favoriteGamecharacter = Keyboard.nextLine();
+        System.out.println("Oh they are pretty cool.");
+
+        System.out.println("What is your favorite movie series");
+        favoriteMovieseries = Keyboard.nextLine();
+        System.out.println("Oh they have put out some good movies.");
+
+        System.out.println("What is better, DC or Marvel?");
+        Answer = Keyboard.nextLine();
+        System.out.println("Ehhhhh");
+
+
+        System.out.println("Did you play legos as a kid?");
+        Answer = Keyboard.nextLine();
+        System.out.println("Oh come on, thats lame");
+
+        System.out.println("Would you rather be able to read minds or have super strength?");
+        Answer = Keyboard.nextLine();
+        System.out.println("Ok, good choice.");
+
+        //Phase 3
+
+        System.out.println("Name a store");
+        store = Keyboard.nextLine();
+        System.out.println("name a bird");
+        bird = Keyboard.nextLine();
+        System.out.println("name your favorite movie series");
+        favoriteMovieseries = Keyboard.nextLine();
+        System.out.println("name a color");
+        color = Keyboard.nextLine();
+        System.out.println("name your favorite car");
+        favoriteCar = Keyboard.nextLine();
+        System.out.println("Name your favorite actor");
+        favoriteActor = Keyboard.nextLine();
+        System.out.println("Name a vegetable" );
+        vegetable = Keyboard.nextLine();
+        System.out.println("Name your favorite drink");
+        drink = Keyboard.nextLine();
+        System.out.println("Name your favorite food");
+        favoriteFood = Keyboard.nextLine();
+        System.out.println("Name a country");
+        country = Keyboard.nextLine();
+        System.out.println("name your favorite rapper");
+        favoriteRapper = Keyboard.nextLine();
+        System.out.println("Name a cologne");
+        cologne = Keyboard.nextLine();
+        System.out.println("name an animal");
+        animal = Keyboard.nextLine();
+        System.out.println("Name the best shoe brand");
+        bestShoebrand = Keyboard.nextLine();
+        System.out.println("name a perfume");
+        perfume = Keyboard.nextLine();
+        System.out.println("Name your favorite comedian");
+        favoriteComedian = Keyboard.nextLine();
+        System.out.println("name your favorite video game");
+        favoriteVideogame = Keyboard.nextLine();
+        System.out.println("Name a meat");
+        meat = Keyboard.nextLine();
+        System.out.println("Name your favorite restaurant");
+        favoriteRestaurant = Keyboard.nextLine();
+        System.out.println("Name a fish");
+        fish = Keyboard.nextLine();
+
+
+
+        System.out.println("One day you are walking to a " + store + " and you see a " + bird + ". You feel as if its going to be a good day because its your favorite bird!");
+        System.out.println("Then out of nowhere you see "+favoriteActor+" get out of a "+color+" "+favoriteCar+ " and go to a park. When you get to the store you decide to get all of " +favoriteMovieseries);
+        System.out.println("You also grab some "+favoriteFood+" and some "+drink+ ".When you were heading to leave you see "+favoriteRapper+" in line with you and you ask for a autograph, then out of ");
+        System.out.println("no where he turns into a " + vegetable + " and begins to float and spin in the air! Then he teleports to what you think is " + country + ". ");
+        System.out.println("As soon as you saw that you ran out the store without even paying. When you are trying to get to get back to your home a angry " + animal + " is blocking your path!");
+        System.out.println("You don't know what to do! But then you come up with an idea! you pull out the " + cologne + "you always carry with you and you start spraying!");
+        System.out.println("The " + perfume + " doesn't seem to be working! Then you just throw your " + bestShoebrand + " shoes at it and it runs away. When you get home you run inside and lock the doors.");
+        System.out.println("You sit down and load up "+favoriteVideogame+" and play a few matches. Then you hear your door bell ring and you go to answer the door. Its "+favoriteComedian+" and he is looking for");
+        System.out.println(" The nearest " +favoriteRestaurant+ ".After this exciting day you feel as if you deserve some relaxation so you put on the movies you bought at some " +meat+ " and" +fish+ ".");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
